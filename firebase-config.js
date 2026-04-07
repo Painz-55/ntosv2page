de34850c8fe9d4ecf47a0a12d76b40a1c88c468c
Painz-55 <xplaykays@gmail.com>
2026-04-07 11:23:18 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,29 +1,16 @@
-// PREENCHA COM AS SUAS CHAVES DO FIREBASE
-// Ative no Firebase:
-// 1) Authentication > Sign-in method > Email/Password
-// 2) Realtime Database
-// 3) Hospede no GitHub Pages normalmente
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDPnnJRWdKzjVEIaQJjoORtuA_q9kg6sXs",
-  authDomain: "databasesitebr2.firebaseapp.com",
-  databaseURL: "https://databasesitebr2-default-rtdb.firebaseio.com/",
-  projectId: "databasesitebr2",
-  storageBucket: "databasesitebr2.firebasestorage.app",
-  messagingSenderId: "354607115653",
-  appId: "1:354607115653:web:17a30122d7487fd45ee8ec"
+export const firebaseSettings = {
+  apiKey: "AIzaSyDc1xAAv-6Namx8bACRS-tsAEpZHiZZE74",
+  authDomain: "guildsitentosv2.firebaseapp.com",
+  databaseURL: "https://guildsitentosv2-default-rtdb.firebaseio.com",
+  projectId: "guildsitentosv2",
+  storageBucket: "guildsitentosv2.firebasestorage.app",
+  messagingSenderId: "1070647907884",
+  appId: "1:1070647907884:web:7897f7a576f917b5cbe389"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
-
-window.firebaseApp = app;
-window.firebaseAuth = auth;
-window.firebaseDb = db;
-
-window.discordWebhookUrl = "https://discord.com/api/webhooks/1490706645095944312/I8vrUtznskwta0wY-H4KWDYU719iSp_czMCigGRv99vZmVlCN0xq_GICi2cB5Hl7vvrB";
+export const appSettings = {
+  discordWebhookUrl: "https://discord.com/api/webhooks/1491080892825735298/Vt0n21dFaLlY77fAWe-orMCauam8cyghUhH_qNjny_iBwgPJ_Uhn0d2BdGt-D6Y7tw_W",
+  rankingRefreshMessage: "Atualizado",
+  defaultTabForAdmin: "newsSection",
+  defaultTabForUser: "newsSection"
+};
