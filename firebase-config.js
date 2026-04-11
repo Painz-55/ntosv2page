296edd2b2fd0e53cc6bed9d15d34f9f059ce16a1
Painz-55 <xplaykays@gmail.com>
2026-04-11 20:11:50 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -10,6 +10,7 @@ export const firebaseSettings = {
 
 export const appSettings = {
   discordWebhookUrl: "https://discord.com/api/webhooks/1491080892825735298/Vt0n21dFaLlY77fAWe-orMCauam8cyghUhH_qNjny_iBwgPJ_Uhn0d2BdGt-D6Y7tw_W",
+  discordApprovalWebhookUrl: "https://discord.com/api/webhooks/1492599363162083410/ma13W_s2OnXU-82Kj4vKqJgzl_IAhQoNvS82nqfAScVsbn36id4J-DDHh_FIQYpnd7zd",
   rankingRefreshMessage: "Atualizado",
   defaultTabForAdmin: "newsSection",
   defaultTabForUser: "newsSection"
